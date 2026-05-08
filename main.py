dff43103bd5284a2f9eea0f31f388f82e2dad1fd
Gustavo da silva sena <gustavo.s.silva097@gmail.com>
2026-05-08 11:08:40 -0300

    Rota Cadastro e login

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ app = FastAPI(title="SIstema de login simples")
 
 #Roda o codigo:
 # python -m uvicorn main:app --reload
+
+templates = Jinja2Templates(directory="templates")
+
+# Rota - método HHTP (get, post)
+
+@app.get("/cadastro")
+def tela_cadastro(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "cadastro.html",
+        {"request": request}
+    )
+
+# Tala de login
+@app.get("/login")
+def tela_login(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"request": request}
+    )
